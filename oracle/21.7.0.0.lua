7b1ae416820f917a357f3b39c3f9e36272cee11e
Neal Soderquist <neals@stanford.edu>
2022-10-21 11:07:22 -0700

    adding oracle client

---
+++ oracle/21.7.0.0.lua
@@ -0,0 +1,9 @@
+whatis("Name:        Oracle CX Client Library")
+whatis("Version:     21.7.0.0")
+whatis("Category:    sql, oracle, database")
+whatis("Keywords:    sql, oracle, database")
+whatis("URL:         https://www.oracle.com/technetwork/topics/linuxx86-64soft-092277.html")
+whatis("Description: Oracle CX_Client for linux")
+
+prepend_path("LD_LIBRARY_PATH", "/share/sw/open/oracle/21.7.0.0")
+
