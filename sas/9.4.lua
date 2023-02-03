2c92c4d7680ee5a375971f63a72dfa5a16cf8788
Neal Soderquist <neals@stanford.edu>
2023-02-03 13:28:23 -0800

    adjusting sas path

--- sas/9.4.lua
+++ sas/9.4.lua
@@ -5,4 +5,4 @@ whatis("Keywords:    application, interpreter, language, statistics")
 whatis("URL:         http://www.sas.com/en_us/software/sas9.html")
 whatis("Description: Advanced analytics, multivariate analyses, business intelligence, data management, and predictive analytics.")
 
-prepend_path("PATH","/share/sw/restricted/sas/9.4/SASHome/SASFoundation/9.4")
+prepend_path("PATH","/share/sw/restricted/sas/usr/local/SASHome/SASFoundation/9.4")
