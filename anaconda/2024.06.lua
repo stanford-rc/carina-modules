a4495946179db1dc1daf1152b54eac9f01053cea
Minh Van <mvan@stanford.edu>
2024-09-24 14:27:55 -0700

    Adding anaconda/2024.06.lua

---
+++ anaconda/2024.06.lua
@@ -0,0 +1,8 @@
+whatis("Name:        Anaconda3")
+whatis("Version:     2024.06")
+whatis("Category:    applications, interpreters, languages")
+whatis("Keywords:    application, interpreter, language")
+whatis("URL:         https://www.anaconda.com/download/#linux")
+whatis("Description: Python 3 distribution")
+
+prepend_path("PATH","/share/sw/open/anaconda3/2024.06/bin/")
