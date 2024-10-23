49ee24d0fe0d6e7ebcebd82b907e0f2a209f3f38
Minh Van <mvan@stanford.edu>
2024-10-22 17:03:31 -0700

    Unhidden 2024.06 ananconda and add lua files for micromamba and mamba

---
+++ mamba/3.lua
@@ -0,0 +1,8 @@
+whatis("Name:        miniforge")
+whatis("Version:     3")
+whatis("Category:    applications, interpreters, languages")
+whatis("Keywords:    application, interpreter, language")
+whatis("URL:         https://mamba.readthedocs.io/en/latest/index.html")
+whatis("Description: Package manager")
+
+prepend_path("PATH","/share/sw/open/mamba/miniforge3/bin")
---
+++ micromamba/2.0.2.lua
@@ -0,0 +1,8 @@
+whatis("Name:        micromamba")
+whatis("Version:     2.0.2")
+whatis("Category:    applications, interpreters, languages")
+whatis("Keywords:    application, interpreter, language")
+whatis("URL:         https://mamba.readthedocs.io/en/latest/index.html")
+whatis("Description: Package manager")
+
+prepend_path("PATH","/share/sw/open/micromamba/bin/")
