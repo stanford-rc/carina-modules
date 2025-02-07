56471a8e3d644d01c624da242ca410f373d42742
David Hummel <ddh@stanford.edu>
2025-02-07 14:03:00 -0500

    Add sas/9.4m{3,8}.lua .

--- sas/9.4m8.lua
+++ sas/9.4m8.lua
@@ -1,8 +1,8 @@
 whatis("Name:        Statistical Analysis System")
-whatis("Version:     9.4 M3")
+whatis("Version:     9.4 M8")
 whatis("Category:    applications, interpreters, languages, statistics, licensed")
 whatis("Keywords:    application, interpreter, language, statistics")
 whatis("URL:         http://www.sas.com/en_us/software/sas9.html")
 whatis("Description: Advanced analytics, multivariate analyses, business intelligence, data management, and predictive analytics.")
 
-prepend_path("PATH","/share/sw/restricted/sas/9.4/SASHome/SASFoundation/9.4")
+prepend_path("PATH","/share/sw/restricted/sas/9.4m8/SASHome/SASFoundation/9.4")
