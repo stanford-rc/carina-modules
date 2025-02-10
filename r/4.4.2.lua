71e55ef982cbf8341c60978e52a021691e120848
David Hummel <ddh@stanford.edu>
2025-02-10 17:59:36 -0500

    Add r/4.4.2.lua .

---
+++ r/4.4.2.lua
@@ -0,0 +1,6 @@
+whatis("Name: R")
+whatis("Version: 4.4.2")
+whatis("Category: tools")
+whatis("URL: https://cran.microsoft.com/")
+whatis("Description: R")
+prepend_path("PATH", "/share/sw/open/R/4.4.2/bin")
